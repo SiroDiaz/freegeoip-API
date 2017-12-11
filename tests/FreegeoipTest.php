22dde10492b88e139bc61b74a8e2b57925629c6d
Siro Diaz <sirodiaz93@gmail.com>
2017-12-11 18:16:50 +0100

    Added tests for json, xml and csv response format

--- tests/FreegeoipTest.php
+++ tests/FreegeoipTest.php
@@ -14,8 +14,51 @@ class FreegeoipTest extends TestCase
         ]);
     }
 
-    public function testJson()
+    public function testJsonOkResponse()
     {
-        $this->assertEquals(true, true);
+        $response = $this->api->json('184.34.65.120');
+        $this->assertObjectHasAttribute('ip', $response);
+        $this->assertObjectHasAttribute('countryCode', $response);
+        $this->assertObjectHasAttribute('countryName', $response);
+        $this->assertObjectHasAttribute('regionCode', $response);
+        $this->assertObjectHasAttribute('regionName', $response);
+        $this->assertObjectHasAttribute('city', $response);
+        $this->assertObjectHasAttribute('zipCode', $response);
+        $this->assertObjectHasAttribute('timeZone', $response);
+        $this->assertObjectHasAttribute('latitude', $response);
+        $this->assertObjectHasAttribute('longitude', $response);
+        $this->assertObjectHasAttribute('metroCode', $response);
+    }
+
+    public function testXmlOkResponse()
+    {
+        $response = $this->api->xml('184.34.65.120');
+        $this->assertObjectHasAttribute('ip', $response);
+        $this->assertObjectHasAttribute('countryCode', $response);
+        $this->assertObjectHasAttribute('countryName', $response);
+        $this->assertObjectHasAttribute('regionCode', $response);
+        $this->assertObjectHasAttribute('regionName', $response);
+        $this->assertObjectHasAttribute('city', $response);
+        $this->assertObjectHasAttribute('zipCode', $response);
+        $this->assertObjectHasAttribute('timeZone', $response);
+        $this->assertObjectHasAttribute('latitude', $response);
+        $this->assertObjectHasAttribute('longitude', $response);
+        $this->assertObjectHasAttribute('metroCode', $response);
+    }
+
+    public function testCsvOkResponse()
+    {
+        $response = $this->api->csv('184.34.65.120');
+        $this->assertObjectHasAttribute('ip', $response);
+        $this->assertObjectHasAttribute('countryCode', $response);
+        $this->assertObjectHasAttribute('countryName', $response);
+        $this->assertObjectHasAttribute('regionCode', $response);
+        $this->assertObjectHasAttribute('regionName', $response);
+        $this->assertObjectHasAttribute('city', $response);
+        $this->assertObjectHasAttribute('zipCode', $response);
+        $this->assertObjectHasAttribute('timeZone', $response);
+        $this->assertObjectHasAttribute('latitude', $response);
+        $this->assertObjectHasAttribute('longitude', $response);
+        $this->assertObjectHasAttribute('metroCode', $response);
     }
 }
